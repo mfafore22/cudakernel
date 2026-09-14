52cb0aff1ec8a8a6419c84f64b346197d4c0c316
mfafore22 <faforemichael001@gmail.com>
2026-09-14 20:09:09 +0300

    added 3d

---
+++ 3dtensor.cu
@@ -0,0 +1,17 @@
+void tensorAdd3D_cpu(
+    const float* A,
+    const float* B,
+    float* c,
+    int depth,
+    int height,
+    int width
+){
+    for (int d = 0; d < depth; ++d){
+        for (int h = 0; h < height; ++h){
+            for (int w = 0; w < width; ++w){
+                int index = d * (height * width) + h * width + w;
+                C[index] = A[index] + B[index];
+            }
+        }
+    }
+}
